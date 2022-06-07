39e1bca6d46a0475171bad3fdbb08d330c82d4ba
edenisova <edenisova@oooinex.ru>
2022-06-07 23:02:08 +0300

    ver 2.3

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,40 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import Home from '../views/Home.vue'
+import freestyle from '@/components/freestyle.vue';
+import editlist from '@/components/edit-list.vue';
+import results from '@/components/appresultstab.vue';
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Freestyle',
+    component: freestyle
+  },
+  {
+    path: '/freestyle',
+    name: 'Freestyle',
+    component: freestyle
+  },
+  {
+    path: '/list', // not ready
+    name: 'List',
+    component: editlist
+  },
+  {
+    path: '/result', // not ready
+    name: 'Results',
+    component: results
   },
   {
     path: '/about',
-    name: 'about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
